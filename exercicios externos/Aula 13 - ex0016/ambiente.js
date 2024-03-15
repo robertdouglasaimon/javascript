2bad55ea7d6a3d460a458ca6ed6455cca3b80058
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2024-03-15 11:25:51 -0300

    Update ambiente.js

--- exercicios externos/Aula 13 - ex0016/ambiente.js
+++ exercicios externos/Aula 13 - ex0016/ambiente.js
@@ -5,3 +5,23 @@ for (var c = 1; c <= 4; c++){
 }
 console.log('FIM!');
 
+// FOR
+/*var c = 1
+for (var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`);
+}*/
+
+// WHILE
+/*var c = 1
+while (c <= 5) {
+    // FOR
+    console.log(`Passo ${c}`);
+    c++
+}*/ 
+
+//DO
+/*var c = 1
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 5)*/
