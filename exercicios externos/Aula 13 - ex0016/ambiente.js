1bfcc03746e1e840697e55a4cd1ded115de93ffe
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2024-03-15 11:22:46 -0300

    UPDATE

--- exercicios externos/Aula 13 - ex0016/ambiente.js
+++ exercicios externos/Aula 13 - ex0016/ambiente.js
@@ -1,7 +1,7 @@
-// DO
+// FOR
+console.log('Vai começar...');
+for (var c = 1; c <= 4; c++){
+    console.log('c');
+}
+console.log('FIM!');
 
-var c = 1
-do {
-    console.log(`Passo ${c}`); // Usando " `` " para poder fazer interpolação por placeholders (para poder usar o ${c} sem problemas, e para ele poder contar usando usando o "c" e o valor que será atribuido a ele, fazendo tipo uma contagem até chegar ao fim do looping).
-    c++ // c = c + 1
-} while (c <= 6) 
