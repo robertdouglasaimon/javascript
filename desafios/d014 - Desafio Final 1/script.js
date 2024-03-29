b7859d48ab48166d28e0048ae26d44cab883f0f4
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2024-03-29 15:11:16 -0300

    UPDATE

--- desafios/d014 - Desafio Final 1/script.js
+++ desafios/d014 - Desafio Final 1/script.js
@@ -6,10 +6,16 @@ let valores = [] //vetor
 
 function adicionar() {    
     if(isNumero(num.value) && !isLista(num.value, valores)) {
-        window.alert('Tudo Ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
        window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
 
 function isNumero(n) {
@@ -28,3 +34,31 @@ function isLista(n, l) {
     }
 } 
 
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        //TESTE LOGICO COM REPETIÇÃO (REVER AULA DE ARRAY PARA FIXAR, TENHO MUITA DIFICULDADE AINDA!)
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                 maior = valores[pos]
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
+}
