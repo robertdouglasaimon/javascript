c67be6daf79fa433ffce78206eb38c274d1c668e
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2024-03-31 01:09:35 -0300

    UPDATE

---
+++ exercicios externos/EXER.CURSO.B7WEB/#8 Exercício de variáveis # 1 (CONCLUIDO)/script.js
@@ -0,0 +1,34 @@
+//1. Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-a.
+
+let nomeDoCarro = "Ferrari"
+console.log(nomeDoCarro)
+
+// 2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-a.
+
+let precoDoBolo = 19.99 //Number
+console.log(precoDoBolo)
+
+// 3. Corrija o código a seguir (e exiba-o):
+// let "cidade" = São Paulo
+
+let cidade = "Sao Paulo"
+console.log(cidade)
+
+cidade = "Brasilia"
+console.log(cidade)
+
+
+/*
+
+function mistura() {
+let nome = window.prompt('Olá, insira seu nome abaixo!')
+if (nome === '') {
+  let res = document.querySelector('div#res');
+  res.innerHTML = 'Insira um valor válido no prompt! Atualize e tente novamente!';
+} else {
+    window.alert(`Viu ${nome}? Deu tudo certo! Como o esperado e parametrado por você nos blocos de JS :D !`);
+    } 
+}
+
+*/
+ 
