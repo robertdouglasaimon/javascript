083a9793d76cb252424ad1437666cdeee3e532d3
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-04-26 22:36:38 -0300

    UPDATE

--- exercicios externos/EXER.CURSO.B7WEB/#13 e 14 Exercício l Resolução Lista de Tarefas Simples (CONCLUIDO)/index.js
+++ exercicios externos/EXER.CURSO.B7WEB/#13 e 14 Exercício l Resolução Lista de Tarefas Simples (CONCLUIDO)/index.js
@@ -1,19 +1,20 @@
 //Elementos do HTML que foram puxados para usarmos no JS.
-let input = document.querySelector('.input');
+let caixaDeTexto = document.querySelector('.caixaDeTexto');
 let lista = document.querySelector('#lista');
 
-//Funções
-function handleKeyUp (e) { 
+
+function adicionarTarefa (e) { 
   if(e.code === 'Enter') {
     //Adicionando uma nova LI na lista
     let novaLi = document.createElement('li');
-    novaLi.innerHTML = input.value;
+    novaLi.innerHTML = caixaDeTexto.value;
     lista.appendChild(novaLi);
     
-    //Esvaziando a caixa após adicionar o evento
-    input.value='';
+
+    caixaDeTexto.value=''; //Esvaziando a caixa após adicionar o evento
   }
 }
 
-//Ouvindo o que o usuário está fazendo no teclado para poder executar a ação da função. Nesse caso aqui, a ação esperada era: Escrever algo e confirmar com ENTER para poder inserir na lista.
-input.addEventListener('keyup', handleKeyUp);
+//Ouvindo o que o usuário está fazendo no teclado para poder executar a ação da função.
+
+caixaDeTexto.addEventListener('keyup', adicionarTarefa);
