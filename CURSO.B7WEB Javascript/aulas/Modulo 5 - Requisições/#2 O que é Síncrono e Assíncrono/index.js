7fefd9d1a924c064ce7c8ba7bcb9714b8110a78f
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-05-16 21:18:01 -0300

    UPDATE

---
+++ CURSO.B7WEB Javascript/aulas/Modulo 5 - Requisições/#2 O que é Síncrono e Assíncrono/index.js
@@ -0,0 +1,11 @@
+/* 
+O que é Sincrono e Assincrono?
+
+Sincrono - Executa por etapas, tanto dependentes quanto não dependentes.
+Assincrono - Executa tudo de uma vez só em etapas.
+*/
+
+let nome = "bonieky";
+let sobrenome = "lacerda";
+
+let nomeCompleto = nome + sobrenome;
