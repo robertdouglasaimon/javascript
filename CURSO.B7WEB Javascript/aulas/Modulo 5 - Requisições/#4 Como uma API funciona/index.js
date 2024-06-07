7c9beded84ab4a8d46b553bfdb3c788bc0848c9d
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-06-07 11:45:00 -0300

    UPDATE

---
+++ CURSO.B7WEB Javascript/aulas/Modulo 5 - Requisições/#4 Como uma API funciona/index.js
@@ -0,0 +1,11 @@
+/* 
+Como a Web funciona (Requisição e Resposta)?
+Request = Requisição
+-> Cabeçalhos / Headers
+-> Corpo / Body
+
+Response = Resposta
+-> Cabeçalhos / Headers
+-> Corpo / Body
+*/
+
---
+++ CURSO.B7WEB Javascript/aulas/Modulo 5 - Requisições/#5 O que é um Callback/index.js
@@ -0,0 +1,5 @@
+document.querySelector('#botao')
+.addEventListener('click', () => {
+    alert("Clicou no botãozinho!");
+});
+
